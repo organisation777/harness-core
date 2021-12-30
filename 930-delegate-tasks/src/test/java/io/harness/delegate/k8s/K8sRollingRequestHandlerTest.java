e9c03d221197f83b02c867c4cae3ee5499bf5d52
Abhinav Singh <abhinav.singh2@harness.io>
2021-12-30 17:07:31 +0530

    [CDS-7467]: k8s rolling deploy with pruning

--- 930-delegate-tasks/src/test/java/io/harness/delegate/k8s/K8sRollingRequestHandlerTest.java
+++ 930-delegate-tasks/src/test/java/io/harness/delegate/k8s/K8sRollingRequestHandlerTest.java
@@ -10,6 +10,7 @@ package io.harness.delegate.k8s;
 import static io.harness.annotations.dev.HarnessTeam.CDP;
 import static io.harness.delegate.k8s.K8sTestHelper.deployment;
 import static io.harness.logging.CommandExecutionStatus.SUCCESS;
+import static io.harness.rule.OwnerRule.ABHINAV2;
 import static io.harness.rule.OwnerRule.ABOSII;
 
 import static java.util.Collections.emptyList;
@@ -39,6 +40,7 @@ import io.harness.delegate.task.k8s.ContainerDeploymentDelegateBaseHelper;
 import io.harness.delegate.task.k8s.K8sDeployResponse;
 import io.harness.delegate.task.k8s.K8sInfraDelegateConfig;
 import io.harness.delegate.task.k8s.K8sRollingDeployRequest;
+import io.harness.delegate.task.k8s.K8sRollingDeployResponse;
 import io.harness.delegate.task.k8s.K8sTaskHelperBase;
 import io.harness.delegate.task.k8s.KustomizeManifestDelegateConfig;
 import io.harness.exception.InvalidRequestException;
@@ -47,9 +49,12 @@ import io.harness.k8s.kubectl.Kubectl;
 import io.harness.k8s.model.K8sDelegateTaskParams;
 import io.harness.k8s.model.KubernetesConfig;
 import io.harness.k8s.model.KubernetesResource;
+import io.harness.k8s.model.KubernetesResourceId;
+import io.harness.k8s.model.Release;
 import io.harness.logging.LogCallback;
 import io.harness.rule.Owner;
 
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.junit.Before;
@@ -59,7 +64,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-
 @OwnedBy(CDP)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class K8sRollingRequestHandlerTest extends CategoryTest {
@@ -68,7 +72,7 @@ public class K8sRollingRequestHandlerTest extends CategoryTest {
   @Mock ContainerDeploymentDelegateBaseHelper containerDeploymentDelegateBaseHelper;
 
   @InjectMocks @Spy K8sRollingBaseHandler baseHandler;
-  @InjectMocks K8sRollingRequestHandler rollingRequestHandler;
+  @InjectMocks @Spy K8sRollingRequestHandler rollingRequestHandler;
 
   @Mock ILogStreamingTaskClient logStreamingTaskClient;
   @Mock LogCallback logCallback;
@@ -152,5 +156,53 @@ public class K8sRollingRequestHandlerTest extends CategoryTest {
         .saveReleaseHistory(any(KubernetesConfig.class), anyString(), anyString(), anyBoolean());
     verify(baseHandler, times(1))
         .addLabelsInDeploymentSelectorForCanary(anyBoolean(), anyBoolean(), anyList(), anyList());
+  }
+
+  @Test
+  @Owner(developers = ABHINAV2)
+  @Category(UnitTests.class)
+  public void testExecuteTaskInternalWithPruning() throws Exception {
+    K8sDelegateTaskParams delegateTaskParams = K8sDelegateTaskParams.builder().build();
+    K8sRollingDeployRequest deployRequestWithPruningEnabled =
+        K8sRollingDeployRequest.builder().releaseName("releaseName").pruningEnabled(true).build();
+    List<KubernetesResourceId> prunedResourceIds = singletonList(KubernetesResourceId.builder().build());
+    doReturn(prunedResourceIds).when(rollingRequestHandler).prune(any(), any(), any());
+
+    K8sDeployResponse deployResponse = rollingRequestHandler.executeTaskInternal(
+        deployRequestWithPruningEnabled, delegateTaskParams, logStreamingTaskClient, commandUnitsProgress);
+    assertThat(((K8sRollingDeployResponse) deployResponse.getK8sNGTaskResponse()).getPrunedResourceIds())
+        .isEqualTo(prunedResourceIds);
+  }
+
+  @Test
+  @Owner(developers = ABHINAV2)
+  @Category(UnitTests.class)
+  public void testPruningWithNoResourceToPrune() throws Exception {
+    assertThat(rollingRequestHandler.prune(null, null, logCallback)).isEmpty();
+
+    Release releaseWithEmptySpecs = Release.builder().resourcesWithSpec(emptyList()).build();
+    assertThat(rollingRequestHandler.prune(null, releaseWithEmptySpecs, logCallback)).isEmpty();
+
+    doReturn(emptyList()).when(taskHelperBase).getResourcesToBePrunedInOrder(any(), any());
+    Release releaseWithDummySpec =
+        Release.builder().resourcesWithSpec(singletonList(KubernetesResource.builder().build())).build();
+    assertThat(rollingRequestHandler.prune(null, releaseWithDummySpec, logCallback)).isEmpty();
+  }
+
+  @Test
+  @Owner(developers = ABHINAV2)
+  @Category(UnitTests.class)
+  public void testPruning() throws Exception {
+    Release releaseWithDummySpec =
+        Release.builder().resourcesWithSpec(singletonList(KubernetesResource.builder().build())).build();
+    List<KubernetesResourceId> toBePruned = singletonList(KubernetesResourceId.builder().build());
+    doReturn(toBePruned)
+        .when(taskHelperBase)
+        .executeDeleteHandlingPartialExecution(any(Kubectl.class), any(K8sDelegateTaskParams.class),
+            anyListOf(KubernetesResourceId.class), any(LogCallback.class), anyBoolean());
+    doReturn(toBePruned)
+        .when(taskHelperBase)
+        .getResourcesToBePrunedInOrder(anyListOf(KubernetesResource.class), anyListOf(KubernetesResource.class));
+    assertThat(rollingRequestHandler.prune(null, releaseWithDummySpec, logCallback)).isEqualTo(toBePruned);
   }
 }
