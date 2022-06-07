d3c9a5f2339ed59f86b2a4505ff4e5f6010f20a0
Shivam Negi <shivam.negi@harness.io>
2022-06-07 13:35:58 +0530

    [CDS-38023]: UT fixes

--- 800-pipeline-service/src/test/java/io/harness/pms/plan/creation/PipelineServiceInternalInfoProviderTest.java
+++ 800-pipeline-service/src/test/java/io/harness/pms/plan/creation/PipelineServiceInternalInfoProviderTest.java
@@ -10,7 +10,6 @@ package io.harness.pms.plan.creation;
 import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 import static io.harness.rule.OwnerRule.NAMAN;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.doNothing;
