42b22e6c08e9d09f3f8e0037c224aa5b79324547
Ved Tambat <ved.atultambat@harness.io>
2022-06-08 19:50:05 +0530

    [CDS-38397]: Amazon Request/Response DTOs

---
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/artifact/resources/amazons3/dtos/AmazonS3RequestDTO.java
@@ -0,0 +1,14 @@
+package io.harness.cdng.artifact.resources.amazons3.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+
+@Value
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AmazonS3RequestDTO {
+}
