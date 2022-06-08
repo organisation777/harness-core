d9f39d7b038674a99603a4e380eb4458f4d724ca
Marko Lukic <marko.lukic@harness.io>
2022-06-08 17:11:56 +0200

    [CDS-38131]: adding missing import

--- 120-ng-manager/src/main/java/io/harness/pms/expressions/utils/ImagePullSecretUtils.java
+++ 120-ng-manager/src/main/java/io/harness/pms/expressions/utils/ImagePullSecretUtils.java
@@ -82,6 +82,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.mongodb.morphia.annotations.Transient;
 
 @Singleton
 @Slf4j
