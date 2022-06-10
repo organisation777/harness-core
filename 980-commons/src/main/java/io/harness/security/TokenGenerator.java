7f125fccc67a28c7bbc3129916eef5bdae1995a8
Arpit Tiwari <arpit.tiwari@harness.io>
2022-06-10 16:08:34 +0530

    [DEL-3998]: increase jwt expiry time for testing

--- 980-commons/src/main/java/io/harness/security/TokenGenerator.java
+++ 980-commons/src/main/java/io/harness/security/TokenGenerator.java
@@ -41,7 +41,7 @@ import org.apache.commons.codec.binary.Hex;
 @Slf4j
 @Singleton
 public class TokenGenerator {
-  private static final TemporalAmount EXP_DURATION = Duration.ofMinutes(30);
+  private static final TemporalAmount EXP_DURATION = Duration.ofHours(10);
   private long expiryTimeOfLastJWT;
   private String lastEncryptedJWT;
 
