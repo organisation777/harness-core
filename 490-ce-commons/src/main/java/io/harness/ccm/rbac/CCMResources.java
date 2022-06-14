145c8fb1f9af94792aa0eb3e8ffce1a5e2cf20bf
Shubhanshu Verma <shubhanshu.verma@harness.io>
2022-06-14 11:27:14 +0530

    [CCM-7452]: Added CCM resources file

---
+++ 490-ce-commons/src/main/java/io/harness/ccm/rbac/CCMResources.java
@@ -0,0 +1,9 @@
+package io.harness.ccm.rbac;
+
+public interface CCMResources {
+  String FOLDER = "FOLDER";
+  String PERSPECTIVE = "PERSPECTIVE";
+  String BUDGET = "BUDGET";
+  String COST_CATEGORY = "COSTCATEGORY";
+  String AUTO_STOPPING_RULE = "AUTOSTOPPINGRULE";
+}
